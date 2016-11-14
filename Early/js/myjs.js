fbdaf173febd791b4b06b6ef4030c11657f3f53d
Алексей Шутко <alexeybelweb@gmail.com>
2016-11-15 01:33:31 +0300

    First

---
+++ Early/js/myjs.js
@@ -0,0 +1,12 @@
+$( document ).ready(function( $ ) {
+		$( '#example3' ).sliderPro({
+			width:1200,
+			height: 480,
+			fade: true,
+			arrows: true,
+			buttons: true,
+            fadeArrows:false,
+			thumbnailArrows: true,
+			autoplay: false
+		});
+	});
